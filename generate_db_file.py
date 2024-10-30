5e05e74fa92da68dd7671e6d11ec6c53a72a6e77
Teo Moreno Piccini <teo.patagonico1.apps@gmail.com>
2024-10-29 22:43:23 -0300

    Modificado el script de Python de generación de la BDD para su correcto funcionamiento con la librería SQLite3.

--- generate_db_file.py
+++ generate_db_file.py
@@ -1,22 +1,26 @@
 import sqlite3
 
-# Path to the SQLite database file
+# Nombre de la BDD
 db_path = 'bdd_usuarios_membresias.db'
 
-# Path to the SQL script
+# Nombre del script de SQL
 sql_script_path = './db_generator_script.sql'
 
-# Connect to the SQLite database (this will create the .db file if it doesn't exist)
+# Conectar a la BDD (si no existe es creada)
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-# Read and execute the SQL script
+# Leer el script SQL
 with open(sql_script_path, 'r') as sql_file:
     sql_script = sql_file.read()
-    cursor.executescript(sql_script)
 
-# Commit the changes and close the connection
+# Separar las instrucciones del script SQL
+sql_script = sql_script.split("-----")
+
+# Ejecutar las instrucciones del script SQL
+for instruccion in sql_script:
+    cursor.execute(instruccion)
+
+# Confirmar cambios y cerrar la conexion
 conn.commit()
 conn.close()
-
-print(f"Database '{db_path}' has been created and populated with data from '{sql_script_path}'.")
